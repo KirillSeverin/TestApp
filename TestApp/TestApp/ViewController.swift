d789d67e50653506d2a78d4dc7568ca9da90d9ae
Kirill Severin <kirillseverin2020@gmail.com>
2024-04-02 22:01:08 +0300

    Created base project, removed storyboard references

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Kirill Severin on 2.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
