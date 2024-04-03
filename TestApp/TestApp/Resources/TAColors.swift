a87827042c43a08848cf75a339d9cb3b3d507f03
Kirill Severin <kirillseverin2020@gmail.com>
2024-04-03 23:25:48 +0300

    Added subscription plan view. Added app colors

---
+++ TestApp/TestApp/Resources/TAColors.swift
@@ -0,0 +1,16 @@
+//
+//  TAColors.swift
+//  TestApp
+//
+//  Created by Kirill Severin on 3.04.24.
+//
+
+import UIKit
+
+enum TAColors {
+    static let subscriptionViewBackground = UIColor().hex(0x1A1A1A)
+    static let subscriptionGray = UIColor().hex(0x8C8C8C)
+    static let green = UIColor().hex(0x25C656)
+    static let white = UIColor().hex(0xFFFFFF)
+    static let subscriptionOldCost = UIColor().hex(0x8C8C8C)
+}
--- TestApp/TestApp/UIlayer/Scenes/Advertisement/AdvertisementViewController.swift
+++ TestApp/TestApp/UIlayer/Scenes/Advertisement/AdvertisementViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class AdvertisementViewController: UIViewController {
+    
+    private let bottomBackgroundView = UIView()
+    private let specialOfferLabel = UILabel()
+    private let lifeTimeSubscriptionView = TASubscriptionView()
+    private let monthlyPlanSubscriptionView = TASubscriptionView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
     }
 
+    
 }
